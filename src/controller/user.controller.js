b0c556a7b7b4f8c6511c5b020fa364b24a27acc3
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-04-24 15:08:56 +0530

    feat: update user registration and login functionality with additional fields and error handling

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,7 @@
 import express from "express"
-import { ApiError } from "../utills/apiError"
-import { ApiResponse } from "../utills/apiResponse"
-import { asyncHandler } from "../utills/asyncHandler"
+import { ApiError } from "../utils/apiError"
+import { ApiResponse } from "../utils/apiResponse"
+import { asyncHandler } from "../utils/asyncHandler"
 import { User } from "../models/user.model"
 
 
@@ -28,10 +28,10 @@ const generateAccessAndRefereshTokens = async(userId) =>{
 
 const registerUser = asyncHandler( async (req, res) => {
 
-    const {fullname, username, email, password} = req.body
+    const {fullname, username, email, password, ganeder, dateOfBirth, phoneNumber} = req.body
 
     if(
-        [fullname, username, email, password].some((field) => field?.trim() === "")
+        [fullname, username, email, password, ganeder, dateOfBirth, phoneNumber].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400, "All field are require.")
     }
@@ -48,7 +48,10 @@ const registerUser = asyncHandler( async (req, res) => {
         fullname,
         email,
         password,
-        username: username.toLowerCase()
+        username: username.toLowerCase(),
+        ganeder, 
+        dateOfBirth, 
+        phoneNumber
 
     })
 
@@ -59,9 +62,84 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     return res.status(201).json(
-        new ApiResponse(200, createdOwner.data, createdOwner.message)
+        new ApiResponse(200, createdUser.data, createdUser.message)
     )
 
 })
 
-export { registerUser }
+
+
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    if (!email) {
+        throw new ApiError(401, "email is require.")
+    }
+
+    const user = await User.findOne({email})
+
+    if (!user) {
+        throw new ApiError(401, "email or password is wrong.")
+    }
+
+    const checkPassword = await user.isPasswordCorrect(password)
+
+    if (!checkPassword) {
+        throw new ApiError(400, "email or password is wrong.")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await user.findById(user._id).select("-password")
+
+    const options = {
+        httpOnly: true,
+        secure: false
+    }
+
+    return res.status(200).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(200, { user: loggedInUser, accessToken, refreshToken}, "user loggedIn successfull."))
+
+})
+
+
+const userProfile = async (req, res) => {
+  return res.status(200).json({
+    success: true,
+    user: req.user,
+  })
+}
+
+
+
+const userlogout = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200,{}, "user logout success."))
+
+})
+
+
+export { registerUser,
+    loginUser,
+    userProfile,
+    userlogout
+ }
