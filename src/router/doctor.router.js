7ac9c9d2420c5718ae185e29a04d2362b3b7c823
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-05-04 19:52:18 +0530

    add router for get Doctor Appointments

--- src/router/doctor.router.js
+++ src/router/doctor.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyDoctorJwt } from "../middleware/autho.middleware";
-import { doctorlogout, doctorProfile, loginDoctor } from "../controller/doctor.controller";
+import { doctorlogout, doctorProfile, getDoctorAppointments, loginDoctor } from "../controller/doctor.controller";
 
 
 
@@ -11,6 +11,7 @@ const router = Router()
 router.route('/doctorlogin').post(loginDoctor)
 router.route('/doctorlogout').post(verifyDoctorJwt, doctorlogout)
 router.route('/doctorProfile').get(verifyDoctorJwt, doctorProfile)
+router.route('/getDoctorAppointments').get(verifyDoctorJwt, getDoctorAppointments)
 
 
 export default router
