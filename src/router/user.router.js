c606e245fe4baebee92b2a283bc19e42123ea2e4
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-05-05 20:07:15 +0530

    add router for cancel Appointment api

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { bookAppointment, loginUser, registerUser, userlogout, userProfile } from "../controller/user.controller";
+import { bookAppointment, cancelAppointment, loginUser, registerUser, userlogout, userProfile } from "../controller/user.controller";
 import { verifyUserJwt } from "../middleware/autho.middleware";
 
 
@@ -13,6 +13,7 @@ router.route('/userlogin').post(loginUser)
 router.route('/userlogout').post(verifyUserJwt, userlogout)
 router.route('/userProfile').get(verifyUserJwt, userProfile)
 router.route('/bookAppointment').post(verifyUserJwt, bookAppointment)
+router.route("/appointments/:id/cancel").post(verifyUserJwt, cancelAppointment);
 
 
 export default router
